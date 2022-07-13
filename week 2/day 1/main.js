c41280162da6389b2d32c7022b8b93ca58cdf173
sanved147 <sanvedak90670@gmail.com>
2022-07-13 12:04:28 +0530

    work

---
+++ week 2/day 1/main.js
@@ -0,0 +1,12 @@
+let name 
+function get() {
+
+let username=document.getElementById("username").value;
+let password=document.getElementById("password").value;
+if((username=="sanved") && (password=="147"))
+{
+alert("***login successful***")
+}
+else
+alert("***login failed****")
+}
